974c2e5b974f8d72bceea24c7c8e5f7a427fee18
preotrix-prabhakar <prabhakar8859@gmail.com>
2024-09-17 19:57:25 +0530

    category navigation done

--- src/app/review/page.tsx
+++ src/app/review/page.tsx
@@ -1,11 +1,9 @@
-
 "use client"
 import React from "react";
 import { useEffect, useState } from "react";
-import '../global.css';
 function page() {
   const [skillsData,setSkillsData]=useState([]);
-
+  const [currentCategoryIndex,setCurrentCategoryIndex]=useState(0);
 useEffect(()=>{
     const questions= async ()=>{
 
@@ -20,12 +18,33 @@ useEffect(()=>{
     }
     questions();
 },[])
+
+const handleNext=()=>{
+  if(currentCategoryIndex < skillsData.length-1)
+  setCurrentCategoryIndex(currentCategoryIndex+1);
+else {
+  <div>No further category...</div>
+}
+}
+const handlePrevious=()=>{
+if(currentCategoryIndex >0){
+  setCurrentCategoryIndex(currentCategoryIndex-1);
+}
+}
+const currentCategory=skillsData[currentCategoryIndex];
   return (
     <div >
-      hello world!!!
+      The Category is:
       <div>
-        {skillsData.map((array,index)=>(<h1 key={index} className="text-blue-600 ">{array.name}</h1> ))}
         
+        {currentCategory && (
+          <h1 className="text-blue-600">
+            {currentCategory.name} {/* Display the first item's name */}
+          </h1>
+        )}
+        
+        <button className="bg-gray-600 mr-5" onClick={handlePrevious} >previous</button>
+        <button className="bg-red-600" onClick={handleNext} >Next</button>
       </div>
     </div>
   )
